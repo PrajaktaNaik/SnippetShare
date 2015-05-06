f0b74763e71d05640b880cc03adf0e2661341a0f
Kunal Barve <kunal.barve@sjsu.edu>
2015-05-06 10:58:27 -0700

    Accept Request
    Accept the request for private board

--- src/main/java/com/cmpe275/snippetshare/DAO/RequestMapperDAO.java
+++ src/main/java/com/cmpe275/snippetshare/DAO/RequestMapperDAO.java
@@ -1,10 +1,13 @@
 package com.cmpe275.snippetshare.DAO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
+import com.cmpe275.snippetshare.Model.Board;
 import com.cmpe275.snippetshare.Model.RequestMapper;
 import com.cmpe275.snippetshare.Utility.MongoConfig;
 
@@ -28,5 +31,34 @@ public class RequestMapperDAO {
 		System.out.println("Mapper's value"+mapper);
 		
 		return mapper;
+	}
+	
+	public static List<RequestMapper> getPendingRequests(String currentUser, String requestStatus)throws Exception{
+		Query query = new Query();
+		query.addCriteria(Criteria.where("ownerId").is(currentUser));
+		query.addCriteria(Criteria.where("status").is(requestStatus));
+		
+		List<RequestMapper> requestList = MongoConfig.getMongoOperationsObj().find(query, RequestMapper.class);
+		
+		return (requestList != null ? requestList : new ArrayList<RequestMapper>());
+	}
+	
+	public static void updateRequest(String requestId, String updatedStatus)throws Exception{
+		Query query = new Query();
+		query.addCriteria(Criteria.where("id").is(requestId));
+		
+		Update update = new Update();
+		update.set("status", updatedStatus);
+		
+		MongoConfig.getMongoOperationsObj().updateFirst(query, update, RequestMapper.class);
+	}
+	
+	public static void updateSharedWith(String boardId, String sharedUser){
+		Query query = new Query(Criteria.where("boardId").is(boardId));
+		
+		Update update = new Update();
+		update.push("sharedWith",sharedUser);
+		
+		MongoConfig.getMongoOperationsObj().findAndModify(query, update, Board.class);
 	}
 }
